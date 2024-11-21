f7f9cd661ac1656b33b248e070a8e61cd67fff85
Tanveer <1905025@ugrad.cse.buet.ac.bd>
2024-11-21 19:57:51 +0600

    offline 4 completed

--- Offline 3/solution/PreProcessors/check.py
+++
@@ -1,5 +0,0 @@
-import sys
-if sys.base_prefix != sys.prefix:
-    print("The code is running inside a virtual environment.")
-else:
-    print("The code is not running inside a virtual environment.")
